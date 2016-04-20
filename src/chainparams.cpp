f7e5d95cc90f66b1e16c6d3d27f4c948673648c4
dima <you@example.com>
2016-04-20 06:36:52 +0200

    amount unfix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -133,7 +133,7 @@ genesis hash: 6989fa140ccf5b8be8a699fa2edb7618ca1a66cfd11367b65df078f438678732
         txNew.vin.resize(1);
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
-        txNew.vout[0].nValue = 100000000 * COIN;
+        txNew.vout[0].nValue = 50 * COIN;
         txNew.vout[0].scriptPubKey = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock = 0;
